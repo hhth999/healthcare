cb652b778c54b872849c940df54638fa9782efe8
LJK <LJK@DESKTOP-7QJQ68C>
2019-04-30 21:09:42 +0900

    커밋

--- healthcare/src/main/java/com/hk/healthcare/HomeController.java
+++ healthcare/src/main/java/com/hk/healthcare/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		System.out.println("변경완료346");
+		System.out.println("변경완료3fdsf46");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
